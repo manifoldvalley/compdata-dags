7b31f73e867b7d81e0f10d1630f0f52166403564
manifold11 <you@example.com>
2023-02-21 12:45:06 -0800

    define catamorphisms for dags

---
+++ src/Data/Comp/Dag/Algebra.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE GADTs               #-}
+{-# LANGUAGE NamedFieldPuns      #-}
+{-# LANGUAGE RankNTypes          #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+
+--------------------------------------------------------------------------------
+-- |
+-- Module      :  Data.Comp.Dag.Algebra
+-- Copyright   :  (c) 2014 Patrick Bahr, Emil Axelsson
+-- License     :  BSD3
+-- Maintainer  :  Patrick Bahr <paba@di.ku.dk>
+-- Stability   :  experimental
+-- Portability :  non-portable (GHC Extensions)
+--
+-- This module implements catamorphisms on dags.
+--
+--------------------------------------------------------------------------------
+
+
+module Data.Comp.Dag.Algebra
+    (
+      cataDag
+    , cataMDag
+    , module I
+    ) where
+
+import Data.Comp.Dag
+import Data.Comp.Dag.Internal
+import Data.Comp.Term
+import Data.Comp.Algebra as I
+import qualified Data.IntMap as IntMap
+
+
+cataDag :: forall f a . Functor f => Alg f a -> Dag f -> a
+cataDag f Dag {root, edges} = f $ fmap run root where
+    run :: Context f Node -> a
+    run (Term t) = f $ fmap run t
+    run (Hole n) = f . fmap run $ edges IntMap.! n
+
+cataMDag :: forall f a m . (Traversable f, Monad m) => AlgM m f a -> Dag f -> m a
+cataMDag f Dag {root, edges} = f =<< mapM run root where
+    run :: Context f Node -> m a
+    run (Term t) = f =<< mapM run t
+    run (Hole n) = f =<< mapM run (edges IntMap.! n)
+
---
+++ src/Data/Comp/Multi/Dag/Algebra.hs
@@ -0,0 +1,49 @@
+{-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE GADTs               #-}
+{-# LANGUAGE NamedFieldPuns      #-}
+{-# LANGUAGE RankNTypes          #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators       #-}
+
+
+--------------------------------------------------------------------------------
+-- |
+-- Module      :  Data.Comp.Dag.AG
+-- Copyright   :  (c) 2014 Patrick Bahr, Emil Axelsson
+-- License     :  BSD3
+-- Maintainer  :  Patrick Bahr <paba@di.ku.dk>
+-- Stability   :  experimental
+-- Portability :  non-portable (GHC Extensions)
+--
+-- This module implements catamorphisms on dags.
+--
+--------------------------------------------------------------------------------
+
+
+module Data.Comp.Multi.Dag.Algebra
+    (
+      cataDag
+    , cataMDag
+    , module I
+    ) where
+
+import Data.Comp.Multi
+import Data.Comp.Multi.Dag
+import Data.Comp.Multi.Dag.Internal
+import Data.Comp.Multi.HTraversable
+import Data.Comp.Multi.Algebra as I
+import qualified Data.Dependent.Map as M
+
+
+cataDag :: forall f a . HFunctor f => Alg f a -> Dag f :-> a
+cataDag f Dag {root, edges} = f $ hfmap run root where
+    run :: Context f Node :-> a
+    run (Term t) = f $ hfmap run t
+    run (Hole n) = f . hfmap run $ edges M.! n
+
+cataMDag :: forall f a m . (HTraversable f, Monad m) => AlgM m f a -> NatM m (Dag f) a
+cataMDag f Dag {root, edges} = f =<< hmapM run root where
+    run :: NatM m (Context f Node) a
+    run (Term t) = f =<< hmapM run t
+    run (Hole n) = f =<< hmapM run (edges M.! n)
+
